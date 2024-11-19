97e8826f5b4afccf28d2cf037e40ee8ba7af78df
Alejandro Arco <alejandroarpu@gmail.com>
2024-11-19 19:54:41 +0100

    feat: first commit por html, css and js. Simple operations in js, a bit of html and css

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = (n1, n2) => {
+  return n1 + n2;
+};
+
+const subtract = (n1, n2) => {
+  return n1 - n2;
+};
+
+const multiply = (n1, n2) => {
+  return n1 * n2;
+};
+
+const divide = (n1, n2) => {
+  return n1 / n2;
+};
+
+const operate = (operator, n1, n2) => {
+  switch (operator) {
+    case "+":
+      return add(n1, n2);
+    case "-":
+      return subtract(n1, n2);
+    case "*":
+      return multiply(n1, n2);
+    case "/":
+      return divide(n1, n2);
+  }
+};
